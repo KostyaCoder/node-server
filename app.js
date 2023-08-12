abbfbf1efa0a4e179911b9bf4f51c0726c4a7b3e
Kostya <forgit266@gmail.com>
2023-08-12 12:56:59 +0000

    Fix app

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const app = express();
-const bodyMes = express.json();
+const bodyParser = express.json();
 const { validateCars } = require("./middlewares/validate.mv");
 const CarsController = require("./controllers/cars.controller");
 
@@ -8,10 +8,10 @@ app.get("/cars", CarsController.getCars);
 
 app.get("/cars/:carId", CarsController.getCar);
 
-app.post("/cars", bodyMes, validateCars, CarsController.createCar);
+app.post("/cars", bodyParser, validateCars, CarsController.createCar);
 
-app.put("/cars/:carId", bodyMes, CarsController.updateCar);
+app.put("/cars/:carId", bodyParser, CarsController.updateCar);
 
-app.delete("/cars/:carId", bodyMes, CarsController.deleteCar);
+app.delete("/cars/:carId", CarsController.deleteCar);
 
 app.listen(5000);
