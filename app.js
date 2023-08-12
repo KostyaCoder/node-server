576dec8adb05c0bdf0d9239065624cd09afcf16f
Kostya <forgit266@gmail.com>
2023-08-12 12:52:53 +0000

    Add routers

--- app.js
+++ app.js
@@ -6,6 +6,12 @@ const CarsController = require("./controllers/cars.controller");
 
 app.get("/cars", CarsController.getCars);
 
+app.get("/cars/:carId", CarsController.getCar);
+
 app.post("/cars", bodyMes, validateCars, CarsController.createCar);
+
+app.put("/cars/:carId", bodyMes, CarsController.updateCar);
+
+app.delete("/cars/:carId", bodyMes, CarsController.deleteCar);
 
 app.listen(5000);
