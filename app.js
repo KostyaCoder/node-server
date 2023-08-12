12da0307db95e965222771ca14e847e17f2b602b
Kostya <forgit266@gmail.com>
2023-08-12 07:21:21 +0000

    Add yup schema

--- app.js
+++ app.js
@@ -3,6 +3,15 @@ const yup = require("yup");
 const app = express();
 const bodyMes = express.json();
 
+const CAR_CREATION_SCHEMA = yup.object({
+  model: yup.string().required(),
+  manufacturer: yup.string().required(),
+  yearIssue: yup.number().required(),
+  engineSize: yup.number().required(),
+  bodyType: yup.string().required(),
+  electricVehicles: yup.boolean().required(),
+});
+
 app.get("/cars", (req, resp) => {
   resp.send("get car");
 });
