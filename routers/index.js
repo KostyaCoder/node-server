0c3e224a281def056ba1fd7fdb485508493b952a
Kostya <forgit266@gmail.com>
2023-08-13 10:00:58 +0000

    Refactor multer

--- routers/index.js
+++ routers/index.js
@@ -1,34 +1,22 @@
-const multer = require("multer");
 const express = require("express");
-const path = require("path");
 const CarsController = require("../controllers/cars.controller");
 const {
   validateCars,
   validateUpdateCar,
 } = require("../middlewares/validate.mv");
+const { uploadeFile } = require("../middlewares/uploadeFile.mv");
 
 const router = express.Router();
-
-const storage = multer.diskStorage({
-  destination: function (req, file, cb) {
-    cb(null, path.resolve(__dirname, "..", "public", "images"));
-  },
-  filename: function (req, file, cb) {
-    cb(null, `${Date.now()}-${file.originalname}`);
-  },
-});
-
-const uploade = multer({ storage });
 
 router
   .route("/cars")
   .get(CarsController.getCars)
-  .post(uploade.single("photo"), validateCars, CarsController.createCar);
+  .post(uploadeFile.single("photo"), validateCars, CarsController.createCar);
 
 router
   .route("/cars/:carId")
   .get(CarsController.getCar)
-  .put(uploade.single("photo"), validateUpdateCar, CarsController.updateCar)
+  .put(uploadeFile.single("photo"), validateUpdateCar, CarsController.updateCar)
   .delete(CarsController.deleteCar);
 
 module.exports = router;
